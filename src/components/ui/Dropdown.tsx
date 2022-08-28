e066ed05dd36b560ff3fa658b60e714c9002838e
JinDouang <jinhda.douang@gmail.com>
2022-08-28 17:41:00 +0200

    refactor(component): #3 add refactor code to have component scss

--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export const Dropdown = ({children, className}: Props) => {
   return (
-    <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+    <div className={`origin-top-right absolute right-0 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none ${className || ''}`}>
          <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-0">Your Profile</a>
          <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-1">Settings</a>
          <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-2">Sign out</a>
--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import { Dropdown } from '../../components/ui/Dropdown';
+import { Profile } from '../../components/menu/Profile/Profile';
+import { NotificationIndicator } from '../../components/ui/NotificationIndicator/Notification';
 import { navLinks } from '../../data/navLinks';
 
 type Props = {
@@ -54,23 +54,12 @@ export const Navbar = ({children, className}: Props) => {
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button type="button" className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-              <span className="sr-only">View notifications</span>
-              {/* <!-- Heroicon name: outline/bell --> */}
-              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-              </svg>
-            </button>
+            {/* <!-- NotificationIndicator -- Heroicon name: outline/bell --> */}
+            <NotificationIndicator/>
     
             {/* <!-- Profile dropdown --> */}
-            <div className="ml-3 relative">
-              <div>
-                <button type="button" className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                  <span className="sr-only">Open user menu</span>
-                  <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-                </button>
-              </div>
-              <Dropdown/>
+            <div className="ml-3">
+              <Profile/>
             </div>
           </div>
         </div>
